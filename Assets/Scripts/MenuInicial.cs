6f5df42005845e4fe1636425a6f8a65a43c1c70c
Bruno <brunors-96@hotmail.com>
2017-10-06 00:01:16 -0300

    Criação do menu incial e menu de pause. #6

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    /*
+     * O metódo irParaACena recebe o nomeDaCena enviado pelo Unity e carrega em SceneManager.LoadScene a cena com o nome passado.
+     */
+    public void irParaACena(string nomeDaCena)
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    private bool pausado = false;
+    public GameObject menuDePause;
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pausar();
+        GameOver();
+    }
+
+    public void irParaACena(string nomeDaCena)
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+
+    private void Pausar()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausado = !pausado;
+        }
+        if (pausado)
+        {
+            menuDePause.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            menuDePause.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Resume()
+    {
+        menuDePause.SetActive(false);
+        Time.timeScale = 1;
+        pausado = !pausado;
+    }
+
+    private void GameOver()
+    {
+        if (player.transform.position.y <= -20)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
